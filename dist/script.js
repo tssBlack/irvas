d7b6b66b841c5c2e2cca5f1a67e885686a4eb3e7
Nikita Takoy <niktatakoy2607@gmail.com>
2025-04-27 20:08:23 +0300

    tabs

--- dist/script.js
+++ dist/script.js
@@ -13827,7 +13827,7 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
   }
   header.addEventListener('click', e => {
     const target = e.target;
-    if (target.classList.contains(tabSelector.replace(/^\./, '')) || target.parentNode.classList.contains(tabSelector.replace(/^\./, ''))) {
+    if (target && (target.classList.contains(tabSelector.replace(/^\./, '')) || target.parentNode.classList.contains(tabSelector.replace(/^\./, '')))) {
       tab.forEach((item, i) => {
         if (target == item || target.parentNode == item) {
           hideTabContent();
@@ -14012,7 +14012,8 @@ __webpack_require__.r(__webpack_exports__);
 
 window.addEventListener('DOMContentLoaded', () => {
   (0,_modules_modals__WEBPACK_IMPORTED_MODULE_1__["default"])();
-  (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])();
+  (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+  (0,_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
 })();
 
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -20,8 +20,8 @@ const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
 
     header.addEventListener('click', (e) => {
         const target = e.target
-        if(target.classList.contains(tabSelector.replace(/^\./, '')) || 
-            target.parentNode.classList.contains(tabSelector.replace(/^\./, ''))) {
+        if(target && (target.classList.contains(tabSelector.replace(/^\./, '')) ||
+            target.parentNode.classList.contains(tabSelector.replace(/^\./, '')))) {
                 tab.forEach((item, i) => {
                     if(target == item || target.parentNode == item) {
                         hideTabContent()
